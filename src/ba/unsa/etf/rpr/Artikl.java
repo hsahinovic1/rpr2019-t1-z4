d8aef6dbadd062c5e526de9f3c0f00b01cdccc33
hsahinovic1 <hsahinovic1@etf.unsa.ba>
2019-10-18 15:35:15 +0200

    Konstruktor i metod postavi u klasi artikal

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String naziv, kod;
+    double cijena;
+    public Artikl(String naziv, String kod, double cijena){postavi(naziv,kod,cijena);};
+    public void postavi(String naziv, String kod, double cijena){this.naziv=naziv; this.kod=kod; this.cijena=cijena; }
 }
