03d55033bdbeb5f49688427f0a42bde31a374931
hsahinovic1 <hsahinovic1@etf.unsa.ba>
2019-10-18 15:41:06 +0200

    Kreirana korpa i metoda dodaj

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] korpa= new Artikl[50];
+    double saldo=0;
+    public void dodajUKorpu(Artikl artikl){ this.korpa[0]=artikl; saldo=saldo+artikl.cijena;}
 }
