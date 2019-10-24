6ed5b26e9ab9e7ad313a45476b5367c6e6a1951d
Hamza Sahinovic <hsahinovic1@etf.unsa.ba>
2019-10-24 23:26:25 +0200

    Metoda dajSaldu koja vraca ukupnu cijenu korpe

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -3,5 +3,6 @@ package ba.unsa.etf.rpr;
 public class Korpa {
     Artikl[] korpa= new Artikl[50];
     double saldo=0;
-    public void dodajUKorpu(Artikl artikl){ this.korpa[0]=artikl; saldo=saldo+artikl.cijena;}
+    public void dodajUKorpu(Artikl artikl){ this.korpa[0]=artikl; saldo=saldo+artikl.dajCijenu();}
+    public double dajSaldu(){return this.saldo;};
 }
