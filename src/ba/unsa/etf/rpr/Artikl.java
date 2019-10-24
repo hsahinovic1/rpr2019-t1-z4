a913b3709ada34ae04634908758b5aa5371e9f1e
Hamza Sahinovic <hsahinovic1@etf.unsa.ba>
2019-10-24 23:18:45 +0200

    Metoda koja vraca cijenu artikla

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -5,4 +5,5 @@ public class Artikl {
     double cijena;
     public Artikl(String naziv, String kod, double cijena){postavi(naziv,kod,cijena);};
     public void postavi(String naziv, String kod, double cijena){this.naziv=naziv; this.kod=kod; this.cijena=cijena; }
+    public double dajCijenu(){return this.cijena;}
 }
