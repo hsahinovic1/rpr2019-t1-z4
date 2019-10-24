3f56352b1702a9fd0f50e0961f7b1b8bc807df0d
Hamza Sahinovic <hsahinovic1@etf.unsa.ba>
2019-10-25 00:00:17 +0200

    Metoda dajUkupnuCijenuArtikal i metod getArtikli

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -3,6 +3,23 @@ package ba.unsa.etf.rpr;
 public class Korpa {
     Artikl[] korpa= new Artikl[50];
     double saldo=0;
-    public void dodajUKorpu(Artikl artikl){ this.korpa[0]=artikl; saldo=saldo+artikl.dajCijenu();}
-    public double dajSaldu(){return this.saldo;};
+   /* public void dodajUKorpu(String kod){
+        int i;
+        Artikl artikl=dajArtikl(kod);
+        for ( i=0;i<50;i++) {
+            if (korpa[i] == null)
+                break;
+        }
+
+        this.korpa[i]=artikl;
+        saldo=saldo+artikl.dajCijenu();}
+    public double dajUkupnuCijenuArtikala(){return this.saldo;};
+    public void izbaciIzKorpe(String kod){
+        Artikl artikl=dajArtikl(kod);
+        for(int i=0; i<50;i++){
+            if(korpa[i].kod.equals(artikl.kod))
+
+        }
+
+    }*/
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,7 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl[] supermarket= new Artikl[1000];
+
+    public Artikl[] getArtikli(){ return this.supermarket;}
 }
